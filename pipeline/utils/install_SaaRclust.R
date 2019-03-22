b463016cd876c96402f4b2de3dddf6fda6b86664
Maryam Ghareghani <mgharegh@d3compute02.mpi-inf.mpg.de>
2019-03-22 18:38:36 +0100

    small adjustments

--- pipeline/utils/install_SaaRclust.R
+++ pipeline/utils/install_SaaRclust.R
@@ -9,4 +9,4 @@ library(devtools)
 source("https://bioconductor.org/biocLite.R")
 biocLite("ComplexHeatmap", lib="utils/R-packages")
 
-withr::with_libpaths(new = "utils/R-packages", install_git("git://github.com/daewoooo/SaaRclust.git", branch = "master"), "prefix")
+withr::with_libpaths(new = "utils/R-packages", install_git("git://github.com/maryamghr/SaaRclust.git", branch = "master"), "prefix")
