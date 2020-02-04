c37e085fced0d630389177074c71ef07989adc26
Maryam Ghareghani <mgharegh@d3compute06.mpi-inf.mpg.de>
2020-02-04 15:23:58 +0100

    adjusted the phasing function fot iterative pb/bubble phasing and added bubble phasing function

--- pipeline/utils/phase_long_reads.snakemake.py
+++ pipeline/utils/phase_long_reads.snakemake.py
@@ -1,3 +1,3 @@
 from phase_long_reads import *
 
-phase_long_reads(snakemake.input[0], snakemake.output[0])
+phase_long_reads(snakemake.input['het_kmers_edit_dist'], snakemake.input['bubble_phase_file'], snakemake.output[0])
