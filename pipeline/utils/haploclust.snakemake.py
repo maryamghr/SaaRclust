e3ad4729ac7376034ae0c1a99f9ad56a6fdbd40d
Maryam Ghareghani <mgharegh@d3compute04.mpi-inf.mpg.de>
2020-07-22 16:04:02 +0200

    small changes

--- pipeline/utils/haploclust.snakemake.py
+++ pipeline/utils/haploclust.snakemake.py
@@ -71,9 +71,10 @@ if __name__ == "__main__":
 	parser.add_argument("--bubbles_haplo_edit_dist_file", type=str, help="The output bubbles haplo edit dist file", required=True)
 	parser.add_argument("--long_reads_haploclust_evaluation_file", type=str, help="The output long reads phasing evaluation file", required=True)
 	parser.add_argument("--long_reads_haplo_edit_dist_file", type=str, help="The output long reads haplo edit dist file", required=True)
-	parser.add_argument("--kmers_file", type=str, help="The output kmers file", required=True)
-	parser.add_argument("--itr", type=str, help="number of iterations for haplotype clustering", required=True)
-	parser.add_argument("--het_kmer_len", type=str, help="The length of heterozygous kmer for computing edit distance", required=True)
+	#parser.add_argument("--kmers_file", type=str, help="The output kmers file", required=True)
+	parser.add_argument("--itr", type=int, help="number of iterations for haplotype clustering", required=True)
+	parser.add_argument("--min_bubbles", type=int, help="minimum number of heterozygous bubbles for haplotype clustering", required=True)
+	parser.add_argument("--het_kmer_len", type=int, help="The length of heterozygous kmer for computing edit distance", required=True)
 	parser.add_argument("--with_km", action='store_true', help="True if bubbles have km information in their name")
 
 	# testing output files for observation 
@@ -87,8 +88,9 @@ if __name__ == "__main__":
 	with_km = True if "with_km" in args else False
 	print('with_km', 	with_km)
 	
-	q=int(args.het_kmer_len)
-	itr=int(args.itr)
+	q=args.het_kmer_len
+	itr=args.itr
+	min_bubbles=args.min_bubbles
 
 	bubbles = get_bubbles(args.bubble_fasta_file, with_km)
 	long_reads = get_long_reads(args.long_reads_fasta_files)
@@ -107,8 +109,8 @@ if __name__ == "__main__":
 	bubble_eval_file_name, long_read_eval_file_name = get_eval_file_names(args.bubbles_haploclust_evaluation_file, args.long_reads_haploclust_evaluation_file)
 
 	print(bubble_eval_file_name, long_read_eval_file_name)	
-	
-	test_bubbles = iterative_haplo_clust(bubbles, long_reads, q, clust_to_chrom, bubble_eval_file_name, long_read_eval_file_name, itr)
+
+	test_bubbles = iterative_haplo_clust(bubbles, long_reads, q, clust_to_chrom, bubble_eval_file_name, long_read_eval_file_name, itr=itr, min_haplotagged_bubbles=min_bubbles)
 	
 	output_phasing(bubbles, long_reads, args.bubble_phase_file, args.long_read_phase_file)
 	output_test_bubbles(test_bubbles, args.test_bubble_phase_file)
@@ -119,7 +121,7 @@ if __name__ == "__main__":
 	evaluate_long_read_clustering(long_reads, args.long_reads_haploclust_evaluation_file)
 	output_long_reads_haplo_dist(long_reads, args.long_reads_haplo_edit_dist_file)
 	# FIXME: It takes a super long time after 100% !!!
-	output_kmers(long_reads, args.kmers_file)
+	##output_kmers(long_reads, args.kmers_file)
 	
 	#output_sampled_long_reads(100, (0.35, 0.4), args.long_reads_with_peak_frac_haplo_edit_dist)
 	#output_sampled_long_reads(10, (0, 0.1), args.long_reads_with_small_frac_haplo_edit_dist)
