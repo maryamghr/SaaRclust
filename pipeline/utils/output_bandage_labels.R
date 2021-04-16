4c22f38117f3e4cb07d5c101f0595320ff33ecec
Maryam Ghareghani <mgharegh@d3compute01.mpi-inf.mpg.de>
2021-04-16 17:07:45 +0200

    adjustments in bandage coloring and evaluation Snakefile

--- pipeline/utils/output_bandage_labels.R
+++ pipeline/utils/output_bandage_labels.R
@@ -6,38 +6,43 @@ library(data.table)
 library(seqinr)
 library(Rsamtools)
 library(reshape2)
-#setwd('aligns_k15_w1_f0.1_z500/SaaRclust_results_HG00733/Clusters/split/hifiasm/')
 
+cat('loading input files\n')
 
-clust_pair <- snakemake@wildcards[['clust_pair']]
-clust.to.chrom <- fread(snakemake@input[["clust_to_chrom_file"]])
-ss.clust <- fread(snakemake@input[["ss_clust"]], col.names=c("SSname_lib", "SSclust"))
+cat('reading ', snakemake@input[["ss_clust"]], '\n')
+ss.clust <- fread(snakemake@input[["ss_clust"]], col.names=c("SSname_lib", "SSclust", "SSchrom.clust"))
+cat('reading ', snakemake@input[["ss_phase"]], '\n')
 ss.phase <- fread(snakemake@input[["ss_phase"]], col.names=c("SSlib", "SSclust", "SShaplo"))
+cat('reading ', snakemake@input[["unitigs_fasta"]], '\n')
 unitigs.fasta <- read.fasta(snakemake@input[["unitigs_fasta"]])
+cat('reading ', snakemake@input[["valid_maps"]], '\n')
 unitig.ss.map <- fread(snakemake@input[["valid_maps"]])
+cat('reading ', snakemake@input[["phased_unitigs"]], '\n')
 unitigs.phase <- fread(snakemake@input[["phased_unitigs"]])
+cat('reading ', snakemake@input[["unitigs_bam"]], '\n')
 unitigs.bam <- scanBam(BamFile(snakemake@input[["unitigs_bam"]]), param=ScanBamParam(what=c("rname", "qname"), tag="HP", flag=scanBamFlag(isSupplementaryAlignment=FALSE)))
+cat('reading ', snakemake@input[["ccs_bam"]], '\n')
 ccs.bam <- scanBam(BamFile(snakemake@input[["ccs_bam"]]), param=ScanBamParam(what=c("rname", "qname"), tag="HP", flag=scanBamFlag(isSupplementaryAlignment=FALSE)))
+cat('reading ', snakemake@input[["hifiasm_gfa"]], '\n')
 hifiasm.gfa <- fread(snakemake@input[["hifiasm_gfa"]], fill=T)
+cat('clust = ', snakemake@wildcards[["clust"]], '\n')
+chr.name <- paste0("chr", snakemake@wildcards[["clust"]])
 
 ################################
 ##### R Consule:
-#clust_pair <- 'V25_V32'
-#clust.to.chrom <- fread('../../../clust_partners.txt')
-#ss.clust <- fread(paste0('../ss_reads/cluster', clust_pair, '.data'), header=F, col.names=c("SSname_lib", "SSclust"))
-#ss.phase <- fread(paste0('phased_strand_states/haplo_strand_states_', clust_pair, '_r.data'), col.names=c("SSlib", "SSclust", "SShaplo"))
-#unitigs.fasta <- read.fasta(paste0(clust_pair,'.r_utg.fa'))
-#unitig.ss.map <- fread(paste0('bubble_ss_map/valid_',clust_pair,'_r_utg_maximal_uniqe_exact_match.data'))
-#unitigs.phase <- fread(paste0('phased_bubbles/iteration1_',clust_pair,'_r.data'))
-#unitigs.bam <- scanBam(BamFile(paste0('bubble_ref_aln/',clust_pair,'.r.haplotagged.bam')), param=ScanBamParam(what=c("rname", "qname"), tag="HP", flag=scanBamFlag(isSupplementaryAlignment=FALSE)))
-#ccs.bam <- scanBam(BamFile(paste0('../HG00733.',clust_pair,'.haplotagged.bam')), param=ScanBamParam(what=c("rname", "qname"), tag="HP", flag=scanBamFlag(isSupplementaryAlignment=FALSE)))
-#hifiasm.gfa <- fread('V25_V32.r_utg.gfa', fill=T)
+#chr.name <- "chr22"
+#ss.clust <- fread('../../HG00733/SaaRclust/Clusters/ss_clusters_22.data', col.names=c("SSname_lib", "SSclust", "SSchrom.clust"))
+#ss.phase <- fread("../../HG00733/phased_strand_states/haplo_strand_states_22.data", col.names=c("SSlib", "SSclust", "SShaplo"))
+#unitigs.fasta <- read.fasta("../../HG00733/hifiasm/split/asm.r_utg_22.fa")
+#unitig.ss.map <- fread("../../HG00733/exact_match/valid_22_r_utg_maximal_uniqe_exact_match.data")
+#unitigs.phase <- fread("../../HG00733/phased_unitigs/22_phased_unitigs.data")
+#unitigs.bam <- scanBam(BamFile("../../HG00733/hifiasm/ref_aln/split/asm.r_utg.haplotagged_22.bam"), param=ScanBamParam(what=c("rname", "qname"), tag="HP", flag=scanBamFlag(isSupplementaryAlignment=FALSE)))
+#ccs.bam <- scanBam(BamFile('../../ccs_reads/split/HG00733_22.haplotagged.bam'), param=ScanBamParam(what=c("rname", "qname"), tag="HP", flag=scanBamFlag(isSupplementaryAlignment=FALSE)))
+#hifiasm.gfa <- fread('../../HG00733/hifiasm/split/asm.r_utg_22.gfa', fill=T)
 ################################
 
+print('input files are loaded.')
 valid.chr <- paste0('chr', c(1:22, "X"))
-clust1 <- strsplit(clust_pair, '_')[[1]][1]
-chr.name <- clust.to.chrom[clust.forward==clust1, original.chrom] #'chr22'
-chr.name <- strsplit(chr.name, '_')[[1]][1]
 
 colnames(unitigs.phase)[1] <- 'unitig_name'
 
@@ -61,9 +66,11 @@ hifiasm.gfa <- hifiasm.gfa[V1=="A", .(V2, V5)]
 colnames(hifiasm.gfa) <- c("unitig_name", "ccs_name")
 #unitigs.gr[, `:=`(ref_haplo=paste0(ref_name, "_", unitig_gr_haplo), ref_name=NULL, unitig_gr_haplo=NULL)]
 
+print('getitng unitigs lens')
 unitig.lens <- sapply(unitigs.fasta, length)
 unitig.lens <- data.table(unitig_name=names(unitig.lens), len=unitig.lens)
 
+print('merging exact match data table with ss haplotypes')
 unitig.ss.cov <- merge(unitig.ss.map, ss.phase[, .(SSname, SShaplo)])
 unitig.ss.cov[, unitig_haplo_ss_cov:=.N, by=.(unitig_name, SShaplo)]
 unitig.ss.cov <- unitig.ss.cov[, head(.SD, 1), by=.(unitig_name, SShaplo, unitig_haplo_ss_cov)]
@@ -71,6 +78,7 @@ unitig.ss.cov <- data.table::dcast(unitig.ss.cov, unitig_name~SShaplo, value.var
 colnames(unitig.ss.cov) <- c("unitig_name", "ss_cov_h1", "ss_cov_h2")
 unitig.ss.cov[, ss_haplo_cov:=paste0("(", ss_cov_h1, ",", ss_cov_h2, ")"), by=unitig_name]
 
+print('merging with ccs ground true haplotypes')
 unitig.ccs.cov <- merge(hifiasm.gfa, ccs.gr[, .(ccs_name, ccs_gr_haplo)])
 unitig.ccs.cov[, unitig_haplo_ccs_cov:=.N, by=.(unitig_name, ccs_gr_haplo)]
 unitig.ccs.cov <- unitig.ccs.cov[, head(.SD, 1), by=.(unitig_name, ccs_gr_haplo, unitig_haplo_ccs_cov)]
@@ -83,6 +91,7 @@ unitigs <- merge(unitigs, unitig.ccs.cov[, .(unitig_name, ccs_haplo_cov)], all=T
 unitigs[is.na(ss_haplo_cov), ss_haplo_cov:="(0,0)"]
 unitigs[is.na(ccs_haplo_cov), ss_haplo_cov:="(0,0)"]
 
+print('merging with unitigs phase')
 unitigs <- merge(unitigs, unitigs.phase[, .(unitig_name, haplotype)], all=TRUE, by='unitig_name')
 unitigs <- merge(unitigs, unitigs.gr, by="unitig_name")
 
@@ -94,7 +103,7 @@ unitigs <- merge(unitigs, unitigs.gr, by="unitig_name")
 unitigs[, Color:="#FFA07A"]
 unitigs[haplotype=="H2", Color:="#87CEEB"]
 #unitigs[is.na(haplotype), Color:="gray"]
-unitigs[haplotype=="None", Color:="gray"]
+unitigs[is.na(haplotype), Color:="gray"]
 unitigs[ref_name!=chr.name, Color:="green"]
 
 fwrite(unitigs[, .(unitig_name, unitig_gr_haplo, Color)], file=snakemake@output[["gr_haplo"]], row.names=F, sep='\t', quote=FALSE)
